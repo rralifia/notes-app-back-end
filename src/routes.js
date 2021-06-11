d396509b8c79cafaa2446e309ddba5e3b0f6fc11
Rahma Alifia <rahma.alifia@prosa.ai>
2021-06-11 22:55:23 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+// Memuat kode konfigurasi routing server seperti menentukan path, method,
+// dan handler yang digunakan.
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    /*
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+    */
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
